c0d8291c0264812ff0366bfaa1caf58e5648bce4
jukafah <jukafah@proton.me>
2023-03-10 18:09:09 -0600

    adds calculations

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import datetime
+from collections import namedtuple
 
 from flask import Flask, render_template
 
@@ -9,7 +10,50 @@ START_DATE = datetime.datetime(2011, 7, 1)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", dynamic_life_experience=get_dynamic_life_experience())
+
+
+def get_dynamic_life_experience():
+    dynamic_life_experience = namedtuple(
+        "DynamicLifeExperience",
+        "health total_experience level energy exp_to_next_level"
+    )
+    total_experience = _get_total_experience()
+    level = _get_level(total_experience)
+    return dynamic_life_experience(
+        _get_health(level),
+        total_experience,
+        _get_level(total_experience),
+        _get_energy(),
+        _get_exp_to_next_level()
+    )
+
+
+def _get_exp_to_next_level():
+    today = _get_today()
+    pre = datetime.datetime(today.year, START_DATE.month, START_DATE.day)
+    post = datetime.datetime(today.year+1, START_DATE.month, START_DATE.day)
+    return ((pre if pre > today else post) - today).days
+
+
+def _get_energy():
+    return 200
+
+
+def _get_health(level: int):
+    return level * 73
+
+
+def _get_total_experience():
+    return (_get_today() - START_DATE).days
+
+
+def _get_level(total_experience: int):
+    return total_experience // 365
+
+
+def _get_today():
+    return datetime.datetime.now()
 
 
 if __name__ == '__main__':
--- tests/app_test.py
+++ tests/app_test.py
@@ -5,5 +5,13 @@ import app
 
 def test_start_date_is_07_1_2011():
     result = app.START_DATE
-    assert_that(result).has_year(2011)
-    assert_that(result).has_month(7)
+    assert_that(result).has_year(2011).has_month(7)
+
+
+def test_dynamic_life_experience_contains_health_level_and_total_exp():
+    result = app.get_dynamic_life_experience()
+    assert_that(result.health).is_greater_than(1)
+    assert_that(result.total_experience).is_greater_than(1)
+    assert_that(result.level).is_greater_than(1)
+    assert_that(result.energy).is_equal_to(200)
+    assert_that(result.exp_to_next_level).is_greater_than(0).is_less_than(366)
