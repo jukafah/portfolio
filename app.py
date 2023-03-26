0a138bc82ab96e97b30e141bc258b06aaba61e7d
jukafah <jukafah@proton.me>
2023-03-26 11:19:22 -0500

    first iteration

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from collections import namedtuple
 
 from flask import Flask, render_template
 
+from constants import CHARACTER_STATS
+
 app = Flask(__name__)
 
 START_DATE = datetime.datetime(2011, 7, 1)
@@ -10,7 +12,11 @@ START_DATE = datetime.datetime(2011, 7, 1)
 
 @app.route('/')
 def index():
-    return render_template("index.html", dynamic_life_experience=get_dynamic_life_experience())
+    return render_template(
+        "index.html",
+        dynamic_life_experience=get_dynamic_life_experience(),
+        character_stats=CHARACTER_STATS,
+    )
 
 
 def get_dynamic_life_experience():
